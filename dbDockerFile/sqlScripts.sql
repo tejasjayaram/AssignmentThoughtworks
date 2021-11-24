368114d5d45ac2ced128a68ae56b33c4a9391e56
Tejas <47742145+tejasjayaram@users.noreply.github.com>
2021-11-24 09:46:34 +0530

    Delete sqlScripts.sql

--- dbDockerFile/sqlScripts.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER 'wiki'@'localhost' IDENTIFIED BY 'THISpasswordSHOULDbeCHANGED';
-CREATE DATABASE wikidatabase;  
-GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost';
-FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'wiki'@'localhost';
-exit
